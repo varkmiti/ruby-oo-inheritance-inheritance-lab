25be68bb5eccb8092803235fbee007b271e1f70c
Mark Viti <+>
2022-06-02 13:51:21 -0400

    commit tree b1175086ca5b5b5f25ba5645211305bcc95fc38f

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
 
+    def initialize
+        @knowledge = []
+    end 
+
+    def learn(stuff)
+        @knowledge << stuff
+    end 
+
+    def knowledge
+        @knowledge
+    end
 end
